14aa51b21e0d18c3ba39bb734605ad726345b801
Yanis Verbeke <yanis88357@gmail.com>
2021-06-20 18:24:39 +0200

    Buildup scenes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public int Score { get; set; }
+    public enum State { MENU, COUNTDOWN, GAME, RESULT };
+    public State CurrentState { get; set; }
+    private GameObject _gamePanel, _menuPanel, _resultPanel, _papersObject;
 
     // Start is called before the first frame update
     void Start()
     {
         Score = 0;
+        CurrentState = State.GAME;
+        _gamePanel = GameObject.Find("GamePanel");
+        _menuPanel = GameObject.Find("MenuPanel");
+        //_resultPanel = GameObject.Find("ResultPanel");
+        _papersObject = GameObject.Find("PapersObject");
+        UpdateCurrentScene();
     }
 
     // Update is called once per frame
@@ -18,5 +27,30 @@ public class GameController : MonoBehaviour
         
     }
 
-
+    void UpdateCurrentScene()
+    {
+        _gamePanel.SetActive(false);
+        _menuPanel.SetActive(false);
+        _papersObject.SetActive(false);
+        //_resultPanel.SetActive(true);
+        switch(CurrentState)
+        {
+            case State.MENU:
+                _menuPanel.SetActive(true);
+                break;
+            case State.COUNTDOWN:
+                _gamePanel.SetActive(true);
+                _papersObject.SetActive(true);
+                break;
+            case State.GAME:
+                _gamePanel.SetActive(true);
+                _papersObject.SetActive(true);
+                break;
+            case State.RESULT:
+                //_resultPanel.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
 }
